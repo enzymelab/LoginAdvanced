f7d33a79e8e5ef8457d2271a057d3afcee9a6447
enzymelab <enzymelab@yahoo.com>
2023-11-10 15:50:03 -0600

    login auth

---
+++ src/App.js
@@ -0,0 +1,31 @@
+
+import React from 'react'
+import Register from './Register';
+
+function App() {
+
+  return (
+    <main className="App">
+      <Register />
+    </main>
+  );
+}
+
+export default App;
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+// import AppContainer from "./components/AppContainer"
+
+// const App = () => {
+//   return (
+//     <>
+//       <Router>
+//         <AppContainer />
+//         <Routes >
+//           {/* <Route path = '/' element={< />}/> */}
+          
+//         </Routes>
+         
+//       </Router>
+//       </>
+//   )
+// }
